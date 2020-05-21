6b80e9b6432a03bf02d1a68dcb95097b3ae2d50f
pokemon98 <55584213+pokemon98@users.noreply.github.com>
2020-05-21 13:22:47 +0300

    new LogIn

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,60 +1,64 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import "../views/Login.css";
-import Response from '../api/mock/resources/loginResponse'
-import { useHistory } from "react-router-dom";
 
-
-
-
-export default function Login() {
-
-  const [password, setPassword] = useState("");
-  const [user, setUser] = useState("");
-  const history = useHistory(); // using the 
-
-  // validation of the form + submit is pressed
-  function handleSubmit(event) {
-    if(!(user.length > 0 
-      && password.length > 0 
-      && Response.userName === user 
-      && Response.password === password)){
-
-      alert('Worng');
-    }else{
-      event.preventDefault();
-      console.log("Sent !!!!!!!!!!!!!!!! ");
+export default class Login extends Component {
+  constructor(props){
+    super(props);
+    this.state = {user:'', password:''};
+    // this.userChange = this.userChange.bind(this);
+    // this.submitUser = this.submitUser.bind(this);
+  }
   
-      //TODO: change acoording to the user 
-      let path = user; 
-      history.push(path);
-    }
+
+  submitUser = (event) =>{
+    alert(this.state.user); 
+    event.preventDefault();
+
   }
 
-  return (
-    <div className="Login">
-      <form onSubmit={handleSubmit}>
-        <FormGroup controlId="user" bsSize="large">
-          <ControlLabel>Username </ControlLabel>
-          <FormControl
-            autoFocus
-            type="text"
-            value={user}
-            onChange={e => setUser(e.target.value)}
-          />
-        </FormGroup>
-        <FormGroup controlId="password" bsSize="large">
-          <ControlLabel>Password</ControlLabel>
-          <FormControl
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            type="password"
-          />
-        </FormGroup>
-        <Button block bsSize="large"  type="submit">
-          Login
-        </Button>
-      </form>
-    </div>
-  );
+  //updates the state values 
+  userChange = (event) =>{
+    this.setState({
+        [event.target.name]:event.target.value
+    });
+  }
+
+
+  render(){
+    return (
+      <div className="Login">
+        <form onSubmit={this.submitUser}>
+
+          <FormGroup controlId="user" bsSize="large">
+            <ControlLabel>Username </ControlLabel>
+            <FormControl
+              name="user"
+              value={this.state.user}
+              onChange={this.userChange}
+              type="text" required
+              placeholder="Enter your user name please"
+            />
+          </FormGroup>
+
+          <FormGroup controlId="password" bsSize="large">
+            <ControlLabel>Password</ControlLabel>
+            <FormControl
+              name="password"
+              value={this.state.password}
+              onChange={this.userChange}
+              placeholder="Enter your password please"
+              type="password" required
+            />
+          </FormGroup>
+
+          <Button block bsSize="large"  type="submit">
+            Login
+          </Button>
+
+        </form>
+      </div>
+    );
+  }
+ 
 }
