5ed8675f8fd616b0b493ff7df45208f052664c43
Naor <NaorbenEvgi@gmail.com>
2020-05-24 18:28:12 +0300

    Remove debug statement

--- src/views/teams/teams.js
+++ src/views/teams/teams.js
@@ -58,8 +58,8 @@ export default function () {
           );
         })}
       </ul>
-      {(authContext.state.user &&
-      authContext.state.user.roles.indexOf("FAN") >= 0) || true? (
+      {authContext.state.user &&
+      authContext.state.user.roles.indexOf("FAN") >= 0 ? (
         <div className={classes.createTeam}>
           <Fab color="secondary" aria-label="add" onClick={handleOpen}>
             <AddIcon />
